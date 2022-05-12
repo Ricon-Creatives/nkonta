14e09f31f8c2bb048210675a6c7026b3d87cf14d
eddiedanqz <eddiedanqz11@gmail.com>
2022-05-12 15:16:24 +0000

    added db configs added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MoneyMan</title>
+        <title>Nkonta</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -42,18 +42,18 @@
                     <span class="sr-only">Nkonta</span>
                     <x-application-logo class="h-20 w-auto" />
                 </a>
-                  <div class="-mr-2 flex items-center md:hidden">
+                 <!-- <div class="-mr-2 flex items-center md:hidden">
                     <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
                       <span class="sr-only">Open main menu</span>
-                      <!-- Heroicon name: outline/menu -->
+                       Heroicon name: outline/menu
                       <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                       </svg>
                     </button>
-                  </div>
+                  </div>-->
                 </div>
               </div>
-              <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+              <div class="md:block md:ml-10 md:pr-4 md:space-x-8">
 
                 @if (Route::has('login'))
 
@@ -81,7 +81,7 @@
             Leaving: "duration-100 ease-in"
               From: "opacity-100 scale-100"
               To: "opacity-0 scale-95"
-          -->
+
           <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
             <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
               <div class="px-5 pt-4 flex items-center justify-between">
@@ -91,7 +91,7 @@
                 <div class="-mr-2">
                   <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span class="sr-only">Close main menu</span>
-                    <!-- Heroicon name: outline/x -->
+
                     <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
@@ -109,7 +109,7 @@
               </div>
               <a href="#" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"> Log in </a>
             </div>
-          </div>
+          </div> -->
         </div>
 
         <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
