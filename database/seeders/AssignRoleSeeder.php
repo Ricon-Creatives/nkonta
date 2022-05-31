84287759e9be7e993b25f878d89cecc418234bfb
eddiedanqz <eddiedanqz11@gmail.com>
2022-05-31 17:41:10 +0000

    added assign_role seeder class

--- database/seeders/AssignRoleSeeder.php
+++ database/seeders/AssignRoleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AssignRoleSeeder extends Seeder
 {
@@ -14,7 +15,7 @@ class AssignRoleSeeder extends Seeder
     public function run()
     {
         DB::table('model_has_roles')->insert([
-            'role_id' => 1,
+            'role_id' => 4,
             'model_type' => 'App\Models\User',
             'model_id' => 1,
         ]);
