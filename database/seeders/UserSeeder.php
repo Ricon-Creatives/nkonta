124c62f6d0bfa61cb50f77f9f25fcece103440c8
eddiedanqz <eddiedanqz11@gmail.com>
2022-05-31 17:25:18 +0000

    added assign_role seeder class

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,17 +16,10 @@ class UserSeeder extends Seeder
     public function run()
     {
        DB::table('users')->insert([
-            'id'  => 1,
             'name' => 'nkonta',
             'username' => 'nkonta11',
             'email' => 'nkonta@gmail.com',
             'password' => Hash::make('nkonta_admin1'),
-        ]);
-
-        DB::table('model_has_roles')->insert([
-            'role_id' => 1,
-            'model_type' => 'App\Models\User',
-            'model_id' => 1,
         ]);
 
     }
