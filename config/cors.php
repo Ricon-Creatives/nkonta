8400581d453f7a56f746b53bdf1fc74b37bd2f67
eddiedanqz <eddiedanqz11@gmail.com>
2022-07-09 12:49:42 +0000

    laravel cors configs

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','connect.facebook.net/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*','*.facebook.net/*'],
 
     'allowed_origins_patterns' => [],
 
