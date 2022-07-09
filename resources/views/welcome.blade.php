d3acae16056c30c16859ef23464cd1a7a9295416
eddiedanqz <eddiedanqz11@gmail.com>
2022-07-09 14:00:44 +0000

    laravel cors configs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
     js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
+    //fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 </script>
        <!-- This example requires Tailwind CSS v2.0+ -->
