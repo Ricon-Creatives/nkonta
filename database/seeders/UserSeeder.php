8e86c162ef668e5f56f8d4dab100a9ddb58ff4ff
eddiedanqz <eddiedanqz11@gmail.com>
2022-07-17 20:18:13 +0000

    added create company to user seeders class

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Company;
+use App\Models\User;
+
 
 class UserSeeder extends Seeder
 {
@@ -23,6 +26,16 @@ class UserSeeder extends Seeder
             'phone' => '0504628244',
             'password' => Hash::make('nkonta_admin1'),
         ]);
+
+        //
+         $user = User::where('email','nkonta@gmail.com')->first();
+         $team    = new Company();
+            $team->owner_id = $user->id;
+            $team->name = $user->company_name;
+            $team->save();
+
+            // team attach alias
+            $user->attachTeam($team);
 
     }
 }
